1cefdc1c42593f622b5948918c0075292a7207d6
Ladeeluc <laura.hatch.lh@gmail.com>
2020-05-29 22:11:13 -0500

    code

--- loops.js
+++ loops.js
@@ -1,12 +1,50 @@
 
 function forLoop(array) {
-    for(let i = 0; i < 25; i++){
-        if (i === 1){ 
-        array.push('I am 1 strange loop.');
-    } else {
-        array.push('I am' + ${i} + 'strange loops.');
-        }
+    for (let i = 0; i < 25; i++) {
+      if (i === 1) {
+        array.push("I am 1 strange loop.")
+      } else {
+        array.push(`I am ${i} strange loops.`)
+      }
     }
-    return array; 
+  
+    return array
+  }
+
+function whileLoop(n){
+    while(n > 0) {
+        console.log(--n)
+    }
+    return "done"
 }
 
+// function doWhileLoop(num){
+//     var i = 0
+     
+//     function incrementVariable(){
+//     do{i = i + 1;
+//         return i;
+//     }
+
+//     do {
+//     console.log ("I run once regardless.");
+//     } while (incrementVariable() < num)
+// }
+
+//console.log(incrementVariable())
+//console.log(num)
+//     do { console.log("I run once regardless.")
+//     } while(let i = 0; i === 0; incrementVariable())
+
+function doWhileLoop(num) {
+	var i = 0;
+
+	function incrementVariable() {
+		i = i + 1;
+    return i;
+	}
+
+  do {
+    console.log("I run once regardless.");
+  } while (incrementVariable() < num);
+}
